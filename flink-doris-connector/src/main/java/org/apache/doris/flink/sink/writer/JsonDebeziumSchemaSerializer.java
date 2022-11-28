1da8f83ff3f0c4b0e8ec9b4644c9d6acd9aebc0a
wudi <>
2022-11-28 14:17:35 +0800

    update

--- flink-doris-connector/src/main/java/org/apache/doris/flink/sink/writer/JsonDebeziumSchemaSerializer.java
+++ flink-doris-connector/src/main/java/org/apache/doris/flink/sink/writer/JsonDebeziumSchemaSerializer.java
@@ -217,11 +217,10 @@ public class JsonDebeziumSchemaSerializer implements DorisRecordSerializer<Strin
             return null;
         }
         String ddl = extractJsonNode(objectMapper.readTree(historyRecord), "ddl");
-        LOG.debug("received debezium ddl :{}", ddl);
+        LOG.info("parse ddl:{}", ddl);
         if (!Objects.isNull(ddl)) {
             //filter add/drop operation
             if (addDropDDLPattern.matcher(ddl).matches()) {
-                LOG.info("parse ddl:{}", ddl);
                 return ddl;
             }
         }
