54f32278c38b431bd9d7f1267759aa119a394119
LiuYaJing0920 <141601709+LiuYaJing0920@users.noreply.github.com>
2023-08-07 11:59:11 +0800

    Update DorisSourceBuilder.java
    To maintain the same standards as Doris Sink

--- flink-doris-connector/src/main/java/org/apache/doris/flink/source/DorisSourceBuilder.java
+++ flink-doris-connector/src/main/java/org/apache/doris/flink/source/DorisSourceBuilder.java
@@ -63,6 +63,9 @@ public class DorisSourceBuilder<OUT> {
     }
 
     public DorisSource<OUT> build() {
+        if(dorisReadOptions == null) {
+            dorisReadOptions = DorisReadOptions.builder().build();
+        }
         return new DorisSource<>(options, readOptions, boundedness, deserializer);
     }
 }
