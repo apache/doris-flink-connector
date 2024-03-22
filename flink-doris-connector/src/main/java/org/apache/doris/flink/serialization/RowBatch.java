8fbcb41393dedeb90010181c46f93ec42680c587
caoliang-web <71004656+caoliang-web@users.noreply.github.com>
2024-03-22 15:45:18 +0800

    [improve]Compatible with reading doris date/datetime arrow type modification (#351)

--- flink-doris-connector/src/main/java/org/apache/doris/flink/serialization/RowBatch.java
+++ flink-doris-connector/src/main/java/org/apache/doris/flink/serialization/RowBatch.java
@@ -22,6 +22,7 @@ import org.apache.flink.util.Preconditions;
 import org.apache.arrow.memory.RootAllocator;
 import org.apache.arrow.vector.BigIntVector;
 import org.apache.arrow.vector.BitVector;
+import org.apache.arrow.vector.DateDayVector;
 import org.apache.arrow.vector.DecimalVector;
 import org.apache.arrow.vector.FieldVector;
 import org.apache.arrow.vector.FixedSizeBinaryVector;
@@ -29,6 +30,7 @@ import org.apache.arrow.vector.Float4Vector;
 import org.apache.arrow.vector.Float8Vector;
 import org.apache.arrow.vector.IntVector;
 import org.apache.arrow.vector.SmallIntVector;
+import org.apache.arrow.vector.TimeStampMicroVector;
 import org.apache.arrow.vector.TinyIntVector;
 import org.apache.arrow.vector.VarBinaryVector;
 import org.apache.arrow.vector.VarCharVector;
@@ -50,8 +52,10 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -274,44 +278,67 @@ public class RowBatch {
                 break;
             case "DATE":
             case "DATEV2":
-                if (!minorType.equals(Types.MinorType.VARCHAR)) {
+                if (!minorType.equals(Types.MinorType.DATEDAY)
+                        && !minorType.equals(Types.MinorType.VARCHAR)) {
                     return false;
                 }
-                VarCharVector date = (VarCharVector) fieldVector;
-                if (date.isNull(rowIndex)) {
-                    addValueToRow(rowIndex, null);
-                    break;
+                if (minorType.equals(Types.MinorType.VARCHAR)) {
+                    VarCharVector date = (VarCharVector) fieldVector;
+                    if (date.isNull(rowIndex)) {
+                        addValueToRow(rowIndex, null);
+                        break;
+                    }
+                    String stringValue = new String(date.get(rowIndex));
+                    LocalDate localDate = LocalDate.parse(stringValue, dateFormatter);
+                    addValueToRow(rowIndex, localDate);
+                } else {
+                    DateDayVector date = (DateDayVector) fieldVector;
+                    if (date.isNull(rowIndex)) {
+                        addValueToRow(rowIndex, null);
+                        break;
+                    }
+                    LocalDate localDate = LocalDate.ofEpochDay(date.get(rowIndex));
+                    addValueToRow(rowIndex, localDate);
                 }
-                String stringValue = new String(date.get(rowIndex));
-                LocalDate localDate = LocalDate.parse(stringValue, dateFormatter);
-                addValueToRow(rowIndex, localDate);
                 break;
             case "DATETIME":
-                if (!minorType.equals(Types.MinorType.VARCHAR)) {
+                if (!minorType.equals(Types.MinorType.TIMESTAMPMICRO)
+                        && !minorType.equals(Types.MinorType.VARCHAR)) {
                     return false;
                 }
-                VarCharVector timeStampSecVector = (VarCharVector) fieldVector;
-                if (timeStampSecVector.isNull(rowIndex)) {
-                    addValueToRow(rowIndex, null);
-                    break;
+                if (minorType.equals(Types.MinorType.VARCHAR)) {
+                    VarCharVector varCharVector = (VarCharVector) fieldVector;
+                    if (varCharVector.isNull(rowIndex)) {
+                        addValueToRow(rowIndex, null);
+                        break;
+                    }
+                    String stringValue = new String(varCharVector.get(rowIndex));
+                    LocalDateTime parse = LocalDateTime.parse(stringValue, dateTimeFormatter);
+                    addValueToRow(rowIndex, parse);
+                } else {
+                    LocalDateTime dateTime = getDateTime(rowIndex, fieldVector);
+                    addValueToRow(rowIndex, dateTime);
                 }
-                stringValue = new String(timeStampSecVector.get(rowIndex));
-                LocalDateTime parse = LocalDateTime.parse(stringValue, dateTimeFormatter);
-                addValueToRow(rowIndex, parse);
                 break;
             case "DATETIMEV2":
-                if (!minorType.equals(Types.MinorType.VARCHAR)) {
+                if (!minorType.equals(Types.MinorType.TIMESTAMPMICRO)
+                        && !minorType.equals(Types.MinorType.VARCHAR)) {
                     return false;
                 }
-                VarCharVector timeStampV2SecVector = (VarCharVector) fieldVector;
-                if (timeStampV2SecVector.isNull(rowIndex)) {
-                    addValueToRow(rowIndex, null);
-                    break;
+                if (minorType.equals(Types.MinorType.VARCHAR)) {
+                    VarCharVector varCharVector = (VarCharVector) fieldVector;
+                    if (varCharVector.isNull(rowIndex)) {
+                        addValueToRow(rowIndex, null);
+                        break;
+                    }
+                    String stringValue = new String(varCharVector.get(rowIndex));
+                    stringValue = completeMilliseconds(stringValue);
+                    LocalDateTime parse = LocalDateTime.parse(stringValue, dateTimeV2Formatter);
+                    addValueToRow(rowIndex, parse);
+                } else {
+                    LocalDateTime dateTime = getDateTime(rowIndex, fieldVector);
+                    addValueToRow(rowIndex, dateTime);
                 }
-                stringValue = new String(timeStampV2SecVector.get(rowIndex));
-                stringValue = completeMilliseconds(stringValue);
-                parse = LocalDateTime.parse(stringValue, dateTimeV2Formatter);
-                addValueToRow(rowIndex, parse);
                 break;
             case "LARGEINT":
                 if (!minorType.equals(Types.MinorType.FIXEDSIZEBINARY)
@@ -342,7 +369,7 @@ public class RowBatch {
                         addValueToRow(rowIndex, null);
                         break;
                     }
-                    stringValue = new String(largeIntVector.get(rowIndex));
+                    String stringValue = new String(largeIntVector.get(rowIndex));
                     BigInteger largeInt = new BigInteger(stringValue);
                     addValueToRow(rowIndex, largeInt);
                     break;
@@ -359,7 +386,7 @@ public class RowBatch {
                     addValueToRow(rowIndex, null);
                     break;
                 }
-                stringValue = new String(varCharVector.get(rowIndex));
+                String stringValue = new String(varCharVector.get(rowIndex));
                 addValueToRow(rowIndex, stringValue);
                 break;
             case "ARRAY":
@@ -407,6 +434,24 @@ public class RowBatch {
                 throw new DorisException(errMsg);
         }
         return true;
+    }
+
+    private LocalDateTime getDateTime(int rowIndex, FieldVector fieldVector) {
+        TimeStampMicroVector vector = (TimeStampMicroVector) fieldVector;
+        if (vector.isNull(rowIndex)) {
+            return null;
+        }
+        long time = vector.get(rowIndex);
+        Instant instant;
+        if (time / 10000000000L == 0) { // datetime(0)
+            instant = Instant.ofEpochSecond(time);
+        } else if (time / 10000000000000L == 0) { // datetime(3)
+            instant = Instant.ofEpochMilli(time);
+        } else { // datetime(6)
+            instant = Instant.ofEpochSecond(time / 1000000, time % 1000000 * 1000);
+        }
+        LocalDateTime dateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        return dateTime;
     }
 
     private String completeMilliseconds(String stringValue) {
