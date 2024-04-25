8658671543046324561aa0279f1240e48c813558
vinlee19 <1401597760@qq.com>
2024-04-25 19:47:16 +0800

    improve code

--- flink-doris-connector/src/main/java/org/apache/doris/flink/sink/DorisSink.java
+++ flink-doris-connector/src/main/java/org/apache/doris/flink/sink/DorisSink.java
@@ -106,12 +106,8 @@ public class DorisSink<IN>
 
     public DorisAbstractWriter getDorisAbstractWriter(
             InitContext initContext, Collection<DorisWriterState> states) {
-        //  maintain compatibility with 'sink.enable.batch-mode' option.
-        if (WriteMode.STREAM_LOAD_BATCH.equals(dorisExecutionOptions.getWriteMode())
-                || dorisExecutionOptions.enableBatchMode()) {
-            return new DorisBatchWriter<>(
-                    initContext, serializer, dorisOptions, dorisReadOptions, dorisExecutionOptions);
-        } else if (WriteMode.STREAM_LOAD.equals(dorisExecutionOptions.getWriteMode())) {
+
+        if (WriteMode.STREAM_LOAD.equals(dorisExecutionOptions.getWriteMode())) {
             return new DorisWriter<>(
                     initContext,
                     states,
@@ -119,6 +115,9 @@ public class DorisSink<IN>
                     dorisOptions,
                     dorisReadOptions,
                     dorisExecutionOptions);
+        } else if (WriteMode.STREAM_LOAD_BATCH.equals(dorisExecutionOptions.getWriteMode())) {
+            return new DorisBatchWriter<>(
+                    initContext, serializer, dorisOptions, dorisReadOptions, dorisExecutionOptions);
         } else if (WriteMode.COPY.equals(dorisExecutionOptions.getWriteMode())) {
             return new DorisCopyWriter(
                     initContext, serializer, dorisOptions, dorisReadOptions, dorisExecutionOptions);
