c3620d9c92fcf024b5dd0bf806441a08eb946d4c
wudi <676366545@qq.com>
2025-01-06 11:31:42 +0800

    Update flink-doris-connector/src/main/java/org/apache/doris/flink/sink/HttpUtil.java

--- flink-doris-connector/src/main/java/org/apache/doris/flink/sink/HttpUtil.java
+++ flink-doris-connector/src/main/java/org/apache/doris/flink/sink/HttpUtil.java
@@ -67,7 +67,6 @@ public class HttpUtil {
                                         .setConnectionRequestTimeout(connectTimeout)
                                         .build())
                         .addInterceptorLast(new RequestContent(true));
-        ;
     }
 
     /**
